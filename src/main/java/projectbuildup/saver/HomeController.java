dc60245d1f7999457c45990c6b1f48793e2894ed
hansol <hansol8701@gmail.com>
2022-09-20 14:02:06 +0900

    project initiation

---
+++ src/main/java/projectbuildup/saver/HomeController.java
@@ -0,0 +1,14 @@
+package projectbuildup.saver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import springfox.documentation.annotations.ApiIgnore;
+
+@Controller
+public class HomeController {
+    @GetMapping("/api/usage")
+    @ApiIgnore
+    public String api() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
---
+++ src/test/java/projectbuildup/saver/SaverApplicationTests.java
@@ -0,0 +1,13 @@
+package projectbuildup.saver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
